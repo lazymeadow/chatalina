f4d28d3c1875c6511170305617ba4b3c368a96e3
audrey <audreywiltsie@outlook.com>
2022-09-11 12:28:48 -0600

    loading :)

--- web-client/src/components/Loading.js
+++ web-client/src/components/Loading.js
@@ -1,6 +1,10 @@
+import './Loading.css'
+
+
 export const Loading = ({message}) => {
 	return (
-		<div>
+		<div className={'Loading'}>
+			<img className={'Loading-spin'} src={'/cat128.png'} alt={'Kitty cat cleaning their butt'} />
 			<h1>Loading...</h1>
 			<p style={{fontStyle: 'italic', whiteSpace: "pre-line"}}>{message}</p>
 		</div>
