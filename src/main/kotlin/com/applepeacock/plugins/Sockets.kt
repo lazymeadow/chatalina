1122def4ee616202f1c5e8963db92adeddf19823
audrey <audreywiltsie@outlook.com>
2023-08-03 21:45:34 -0600

    client logging messages wow

--- src/main/kotlin/com/applepeacock/plugins/Sockets.kt
+++ src/main/kotlin/com/applepeacock/plugins/Sockets.kt
@@ -1,6 +1,7 @@
 package com.applepeacock.plugins
 
 import com.applepeacock.database.Parasites
+import com.applepeacock.http.AuthenticationException
 import com.fasterxml.jackson.annotation.JsonAnyGetter
 import com.fasterxml.jackson.annotation.JsonAnySetter
 import com.fasterxml.jackson.annotation.JsonEnumDefaultValue
@@ -16,6 +17,7 @@ import kotlinx.coroutines.channels.ClosedReceiveChannelException
 import kotlinx.coroutines.channels.ClosedSendChannelException
 import kotlinx.coroutines.launch
 import org.slf4j.LoggerFactory
+import org.slf4j.event.Level
 import java.time.Duration
 import java.util.*
 import java.util.concurrent.atomic.AtomicInteger
@@ -32,20 +34,22 @@ fun Application.configureSockets() {
     routing {
         authenticate("auth-parasite-socket") {
             webSocket("/chat") {
-                val theConnection = ChatSocketConnection(this)// websocketSession
+                val parasiteSession = call.principal<ParasiteSession>() ?: throw AuthenticationException()
+                val theConnection = ChatSocketConnection(this, parasiteSession.id)// websocketSession
+                theConnection.logger.debug("connected")
                 ChatManager.currentSocketConnections.add(theConnection)
 
                 try {
                     for (frame in incoming) {
                         when (frame) {
                             is Frame.Text -> {
-                                ChatManager.handleMessage(frame.readText())
+                                ChatManager.handleMessage(theConnection, frame.readText())
                             }
                             else -> outgoing.send(Frame.Text("huh???"))
                         }
                     }
                 } catch (e: ClosedReceiveChannelException) {
-                    println("${theConnection.name}: onClose ${closeReason.await()}")
+                    theConnection.logger.debug("${theConnection.name}: onClose ${closeReason.await()}")
                 } catch (e: Throwable) {
                     e.printStackTrace()
                     application.log.error("what the fuck")
@@ -62,9 +66,10 @@ const val CLIENT_VERSION = "4.0.0"
 object ChatManager {
     val currentSocketConnections: MutableSet<ChatSocketConnection> = Collections.synchronizedSet(LinkedHashSet())
 
-    fun handleMessage(body: String) {
+    suspend fun handleMessage(connection: ChatSocketConnection, body: String) {
         val messageBody = defaultMapper.readValue<MessageBody>(body)
-        messageBody.type.handler.handleMessage(messageBody)
+        connection.logger.debug("received message: ${messageBody.type}")
+        messageBody.type.handler.handleMessage(this, connection, messageBody)
     }
 }
 
@@ -86,7 +91,7 @@ open class MessageBody(
 
 interface MessageHandler {
 
-    fun handleMessage(body: MessageBody)
+    suspend fun handleMessage(manager: ChatManager, connection: ChatSocketConnection, body: MessageBody)
 }
 
 inline fun <reified T : MessageBody> MessageHandler.onMessage(
@@ -97,9 +102,9 @@ inline fun <reified T : MessageBody> MessageHandler.onMessage(
 }
 
 object UnknownMessageHandler : MessageHandler {
-    override fun handleMessage(body: MessageBody) {
+    override suspend fun handleMessage(manager: ChatManager, connection: ChatSocketConnection, body: MessageBody) {
         onMessage<MessageBody>(body) {
-            println("unknown message")
+            connection.logger.error("Received unknown message")
         }
     }
 }
@@ -111,21 +116,65 @@ object VersionMessageHandler : MessageHandler {
         val clientVersion by fromOther("client version")
     }
 
-    override fun handleMessage(body: MessageBody) {
+    override suspend fun handleMessage(manager: ChatManager, connection: ChatSocketConnection, body: MessageBody) {
         onMessage<VersionMessageBody>(body) { messageBody ->
-            if (messageBody.clientVersion.toString() < CLIENT_VERSION) {
-                // TODO: how to send these back through the socket
-                println("Your client is out of date. You\\'d better refresh your page!")
+            val message = if (messageBody.clientVersion.toString() < CLIENT_VERSION) {
+                "Your client is out of date. You'd better refresh your page!"
             } else if (messageBody.clientVersion.toString() > CLIENT_VERSION) {
-                println("How did you mess up a perfectly good client version number?")
+                "How did you mess up a perfectly good client version number?"
+            } else {
+                null
+            }
+            message?.let {
+                connection.send(
+                    ServerMessage(ServerMessageTypes.Alert, mapOf("message" to message, "type" to "permanent"))
+                )
             }
         }
     }
 }
 
+object ClientLogMessageHandler : MessageHandler {
+    class LogMessageBody(
+        type: MessageTypes
+    ) : MessageBody(type) {
+        val level by fromOther("level")
+        val log by fromOther("log")
+    }
+
+    override suspend fun handleMessage(manager: ChatManager, connection: ChatSocketConnection, body: MessageBody) {
+        onMessage<LogMessageBody>(body) { messageBody ->
+            messageBody.log?.toString()?.let {
+                val messageToLog = "(${connection.parasite.id}) $it"
+                when (messageBody.level?.toString()?.lowercase()) {
+                    Level.INFO.toString().lowercase() -> connection.logger.info(messageToLog)
+                    Level.ERROR.toString().lowercase() -> connection.logger.error(messageToLog)
+                    Level.DEBUG.toString().lowercase() -> connection.logger.debug(messageToLog)
+                    Level.TRACE.toString().lowercase() -> connection.logger.trace(messageToLog)
+                    Level.WARN.toString().lowercase() -> connection.logger.warn(messageToLog)
+                }
+            }
+        }
+    }
+}
+
+
+enum class ServerMessageTypes(val value: String) {
+    Alert("alert");
+
+    override fun toString(): String {
+        return this.value
+    }
+}
+
+data class ServerMessage(val type: ServerMessageTypes, val data: Map<String, Any?>)
+
 enum class MessageTypes(val value: String) {
-    //    ClientLog("client log"),
-//    ChatMessage("chat message"),
+    ClientLog("client log") {
+        override val handler = ClientLogMessageHandler
+    },
+
+    //    ChatMessage("chat message"),
 //    PrivateMessage("private message"),
 //    Image("image"),
 //    ImageUpload("image upload"),
@@ -154,17 +203,17 @@ enum class MessageTypes(val value: String) {
     }
 }
 
-class ChatSocketConnection(val session: DefaultWebSocketServerSession) {
+class ChatSocketConnection(val session: DefaultWebSocketServerSession, parasiteId: String) {
     companion object {
         var lastId = AtomicInteger(0)
     }
 
     val name = "WS-${lastId.getAndIncrement()}"
     val logger = LoggerFactory.getLogger(name)
-    lateinit var parasite: Parasites.ParasiteObject
+    var parasite: Parasites.ParasiteObject
 
-    fun isParasiteSet(): Boolean {
-        return this::parasite.isInitialized
+    init {
+        parasite = Parasites.DAO.find(parasiteId) ?: throw AuthenticationException()
     }
 
     fun launchForSocket(block: suspend () -> Unit) {
@@ -174,9 +223,9 @@ class ChatSocketConnection(val session: DefaultWebSocketServerSession) {
         }
     }
 
-    suspend fun send(data: String) {
+    suspend fun send(data: Any) {
         try {
-            this.session.send(data)
+            this.session.send(defaultMapper.writeValueAsString(mapOf("data" to data)))
         } catch (e: ClosedSendChannelException) {
             logger.debug("Attempted to send message to closed session $name")
         }
