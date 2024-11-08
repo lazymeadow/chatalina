b6749a1ae0557d8486d926057c7ab20994967df9
audrey <audreywiltsie@outlook.com>
2024-11-07 21:45:20 -0700

    bump client version

--- src/main/kotlin/net/chatalina/plugins/Sockets.kt
+++ src/main/kotlin/net/chatalina/plugins/Sockets.kt
@@ -67,7 +67,7 @@ fun Application.configureSockets() {
     }
 }
 
-const val CLIENT_VERSION = "4.0.0"
+const val CLIENT_VERSION = "4.0.1"
 
 
 class ChatSocketConnection(val session: DefaultWebSocketServerSession, val parasiteId: String) {
