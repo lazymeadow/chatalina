fa21df48163183955cc699b630381b80f2e6a1bc
audrey <audreywiltsie@outlook.com>
2024-11-11 19:27:51 -0700

    oops

--- src/main/kotlin/net/chatalina/http/routes/MainRoutes.kt
+++ src/main/kotlin/net/chatalina/http/routes/MainRoutes.kt
@@ -2,6 +2,7 @@ package net.chatalina.http.routes
 
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
+import io.ktor.server.pebble.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import net.chatalina.database.ParasiteSettings
@@ -19,6 +20,27 @@ fun Route.mainRoutes() {
     emojiSearch()
 }
 
+private suspend fun ApplicationCall.handleMainRoute(getTemplateContent: () -> PebbleContent) {
+    val session = principal<ParasiteSession>() ?: let {
+        throw RedirectException("/logout")
+    }
+    val sessionParasite = Parasites.DAO.find(session.id)?.takeIf { it.active } ?: let {
+        throw RedirectException("/logout")
+    }
+
+    response.cookies.append("id", sessionParasite.id.value)
+    response.cookies.append("email", sessionParasite.email)
+    ParasiteSettings::class.declaredMemberProperties.forEach { prop: KProperty1<ParasiteSettings, *> ->
+        val propValue = if (ParasiteSettings::displayName == prop) {
+            prop.get(sessionParasite.settings)?.toString() ?: sessionParasite.id.value
+        } else {
+            prop.get(sessionParasite.settings).toString()
+        }
+        response.cookies.append(prop.name, propValue)
+    }
+    respond(getTemplateContent())
+}
+
 private fun Route.getMain() {
     get("/") {
         // NOTE: This will only work if the http/proxy server is attaching the query param on mobile user agent detection.
@@ -27,42 +49,13 @@ private fun Route.getMain() {
             throw RedirectException("/m")
         }
 
-        val session = call.principal<ParasiteSession>() ?: let {
-            throw RedirectException("/logout")
-        }
-        val sessionParasite = Parasites.DAO.find(session.id)?.takeIf { it.active } ?: let {
-            throw RedirectException("/logout")
-        }
-
-        call.response.cookies.append("id", sessionParasite.id.value)
-        call.response.cookies.append("email", sessionParasite.email)
-        ParasiteSettings::class.declaredMemberProperties.forEach { prop: KProperty1<ParasiteSettings, *> ->
-            val propValue = if (ParasiteSettings::displayName == prop) {
-                prop.get(sessionParasite.settings)?.toString() ?: sessionParasite.id.value
-            } else {
-                prop.get(sessionParasite.settings).toString()
-            }
-            call.response.cookies.append(prop.name, propValue)
-        }
-
-        call.respond(application.getPebbleContent("chat.html", "emojiList" to EmojiManager.curatedEmojis))
+        call.handleMainRoute { application.getPebbleContent("chat.html", "emojiList" to EmojiManager.curatedEmojis) }
     }
 }
 
 private fun Route.getMobile() {
     get("/m") {
-        val session = call.principal<ParasiteSession>() ?: let {
-            throw RedirectException("/logout")
-        }
-        val sessionParasite = Parasites.DAO.find(session.id)?.takeIf { it.active } ?: let {
-            throw RedirectException("/logout")
-        }
-        // it only sets the cookies for the items you can change :/ so weird
-        call.response.cookies.append("volume", sessionParasite.settings.volume)
-        call.response.cookies.append("soundSet", sessionParasite.settings.soundSet)
-        call.response.cookies.append("id", sessionParasite.id.value)
-
-        call.respond(application.getPebbleContent("mobile.html", "emojiList" to EmojiManager.curatedEmojis))
+        call.handleMainRoute { application.getPebbleContent("mobile.html") }
     }
 }
 
